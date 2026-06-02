76bda62634b2456ab0e5b60210a372f97d25281b
Shivam <choudharyshivam0508@gmail.com>
2026-06-02 22:38:33 +0530

    feat: add ApiResponse utility class for standardized response formatting

--- Backend/src/utils/ApiResponse.js
+++ Backend/src/utils/ApiResponse.js
@@ -4,7 +4,6 @@ class ApiResponse {
         data,
         message = "Success"
     ){
-        super(message);
         this.statusCode = statusCode >= 200 && statusCode < 400;
         this.data = data;
         this.message = message
