fa84364d16e21d12097ad9028a29c5fd807f449a
Shivam <choudharyshivam0508@gmail.com>
2026-06-02 22:11:47 +0530

    feat: initialize backend project structure with Express, required dependencies, and base user/project routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,22 @@
 import express, { urlencoded } from 'express';
 import cors from 'cors';
+import cokkieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(cors());
+app.use(cookieParser());
 
 app.use(express({limit : '16kb'}));
 app.use(urlencoded());
+
+
+// ? Routes Import 
+import userRouter from './routes/user.route.js';
+import projectRouter from './routes/project.route.js'
+import cookieParser from 'cookie-parser';
+
+app.use("/api/v1/user" , userRouter);
+app.use("/api/v1/project" , projectRouter)
 
 export default app;
