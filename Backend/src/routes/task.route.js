294f0064fae159ef1784a1f88a0458239b4560c0
Shivam <choudharyshivam0508@gmail.com>
2026-06-03 15:29:51 +0530

    feat: create task routes for project management and individual task operations

--- Backend/src/routes/task.route.js
+++ Backend/src/routes/task.route.js
@@ -1,5 +1,20 @@
 import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+import { createTask, getTasks, getTaskById, updateTask, deleteTask, toggleTaskStatus } from "../controller/task.controller.js";
 
 const router = Router();
+
+router.route("/project/:projectId")
+  .post(verifyJWT, createTask)
+  .get(verifyJWT, getTasks);
+
+router.route("/:taskId")
+  .get(verifyJWT, getTaskById)
+  .patch(verifyJWT, updateTask)
+  .delete(verifyJWT, deleteTask);
+
+
+router.route("/:taskId/toggle-status").patch(verifyJWT, toggleTaskStatus);
 
 export default router;
