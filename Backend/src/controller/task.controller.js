caec8d87d05883471cdc0ec66820c91c2feec69e
Shivam <choudharyshivam0508@gmail.com>
2026-06-03 14:37:57 +0530

    feat: implement task controller with validation, creation, and retrieval logic

--- Backend/src/controller/task.controller.js
+++ Backend/src/controller/task.controller.js
@@ -6,20 +6,29 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
-const createTask = asyncHandler(async (req , res) => {
+const validateProject = async (req , res) => {
+    try {
     const projectId = req.params.id;
     if(!projectId){
         throw new ApiError(404 , "Project Id not found...")
     }
-    const project = await Project.findById({projectId});
+    const project = await Project.findById(projectId);
     if(!project){
         throw new ApiError(404 , "Project not found...")
     }
     const user = req.user._id;
-    if(project.id.toString() !== user.id.toString){
+    if(project.user.toString() !== user.id.toString){
         throw new ApiError(403 , "You are not authorized to add task in this project...")
     }
 
+    return { project }
+    } catch (error) {
+       throw error
+    }
+}
+
+const createTask = asyncHandler(async (req ) => {
+    const { project } = await validateProject();
     const { title } = req.body;
     if(!title){
         throw new ApiError(400 , "Task cannot be empty...")
@@ -35,24 +44,15 @@ const createTask = asyncHandler(async (req , res) => {
 });
 
 const getTasks = asyncHandler(async (req , res) => {
-    const projectId = req.params.id;
-    if(!projectId){
-        throw new ApiError(404 , "Project Id not found...")
-    }
-    const project = await Project.findById({projectId});
-    if(!project){
-        throw new ApiError(404 , "Project not found...")
-    }
-    const user = req.user._id;
-    if(project.id.toString() !== user.id.toString){
-        throw new ApiError(403 , "You are not authorized to get the tasks...")
-    }
+    const { project } = await validateProject();
 
-    const fetchTasks = await Task.find({project}).sort({createdAt : -1});
+    const fetchTasks = await Task.find({project : project._id}).sort({createdAt : -1});
     return res.status(200).json(new ApiResponse(200 , {fetchTasks} , "Tasks Fetched Successfully..."))
 });
 
-const getTaskById = asyncHandler(async (req , res) => {});
+const getTaskById = asyncHandler(async (req , res) => {
+    const { project } = await validateProject();
+});
 
 const updateTask = asyncHandler(async (req , res) => {});
 
