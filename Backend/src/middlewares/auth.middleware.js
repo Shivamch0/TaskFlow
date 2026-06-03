dcb93a9a0bba0519c52f785a6572be92c9211b68
Shivam <choudharyshivam0508@gmail.com>
2026-06-03 11:55:13 +0530

    feat: implement JWT verification middleware for user authentication

---
+++ Backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,44 @@
+import jwt from "jsonwebtoken";
+import { User } from "../model/user.model.js";
+
+// ? Utils Import
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
+
+const verifyJWT = asyncHandler( async (req , res , next) => {
+    try {
+        // ! Needed Token that comes from cookies || header
+        const token = req.cookie?.accessToken || req.header("Authorization")?.replace("Bearer " , "");
+        if(!token){
+            throw new ApiError(401 , "Unauthorized request: Token missing...")
+        }
+        //! verify it with access Token secret 
+        const decodedToken = await jwt.verify(token , process.env.ACCESS_TOKEN_SECRET);
+        if(!decodedToken || !decodedToken._id){
+            throw new ApiError(401 , "Invalid token. Please log in again.")
+        }
+        //! find user with the decoded token id in database
+        const user = await User.findById(decodedToken._id).select(" -password -refreshToken ");
+        if(!user){
+             throw new ApiError(401, "Invalid access token: user not found...");
+        }
+        //! set req.user = user
+        req.user = user;
+        next();
+    } catch (error) {
+         if (error instanceof ApiError) {
+      throw error;
+    }
+
+    if (error.name === "TokenExpiredError") {
+      throw new ApiError(401, "Access token expired. Please refresh token.");
+    }
+    if (error.name === "JsonWebTokenError") {
+      throw new ApiError(401, "Invalid token. Please log in again.");
+    }
+
+    throw new ApiError(401, "Unauthorized. Please log in.");
+    }
+});
+
+export { verifyJWT };
