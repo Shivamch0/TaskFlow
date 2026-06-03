ba4c279f20d5eb0b7475d1122396fb0dc50559ea
Shivam <choudharyshivam0508@gmail.com>
2026-06-03 15:26:47 +0530

    feat: implement CRUD and status toggle controllers for tasks

--- Backend/src/controller/task.controller.js
+++ Backend/src/controller/task.controller.js
@@ -1,5 +1,4 @@
 import { Task } from "../model/task.model.js";
-import { Project } from "../model/project.model.js"
 
 //! Helper Imports
 import { validateProject } from "../helpers/project.helper.js";
@@ -17,11 +16,11 @@ const createTask = asyncHandler(async (req , res) => {
         throw new ApiError(400 , "Task cannot be empty...")
     }
 
-    const { project } = await validateProject(req.params.projectId , req.user._id);
+    const  project  = await validateProject(req.params.projectId , req.user._id);
 
     const task = await Task.create({
         title,
-        project
+        project : project._id
     })
 
     return res.status(200).json(new ApiResponse(200 , {task} , "Task Created Successfully..."))
@@ -29,36 +28,35 @@ const createTask = asyncHandler(async (req , res) => {
 });
 
 const getTasks = asyncHandler(async (req , res) => {
-    const { project } = await validateProject(req.params.projectId , req.user._id);
+    const  project  = await validateProject(req.params.projectId , req.user._id);
 
     const fetchTasks = await Task.find({project : project._id}).sort({createdAt : -1});
     return res.status(200).json(new ApiResponse(200 , {fetchTasks} , "Tasks Fetched Successfully..."))
 });
 
 const getTaskById = asyncHandler(async (req , res) => {
-    const taskId = req.params.id;
+    const taskId = req.params.taskId;
     if(!taskId){
         throw new ApiError(404 , "Task Id not found...")
     }
 
-    await validateProject(req.params.projectId , req.user._id);
+    const task = await validateTask(taskId , req.user._id);
 
-    const fetchTaskById = await Task.findById(taskId);
-
-    return res.status(200).json(new ApiResponse(200 , {fetchTaskById} , "Task fetched by id successfully..."))
+    return res.status(200).json(new ApiResponse(200 , {task} , "Task fetched by id successfully..."))
 
 });
 
 const updateTask = asyncHandler(async (req , res) => {
 
-    const taskId = req.params.id;
+    const {title} = req.body;
+    const taskId = req.params.taskId;
     if(!taskId){
         throw new ApiError(404 , "Task Id not found...")
     }
 
-    await validateProject(req.params.projectId , req.user._id);
+    const task = await validateTask(taskId , req.user._id);
 
-    const updatedTask = await Task.findByIdAndUpdate(taskId , 
+    const updatedTask = await Task.findByIdAndUpdate(task._id , 
         {
             $set : {
                 title
@@ -72,31 +70,26 @@ const updateTask = asyncHandler(async (req , res) => {
 });
 
 const deleteTask = asyncHandler(async (req , res) => {
-    const taskId = req.params.id;
+    const taskId = req.params.taskId;
     if(!taskId){
         throw new ApiError(404 , "Task Id not found...")
     }
 
-    await validateProject(req.params.projectId , req.user._id);
+    const task = await validateTask(taskId , req.user._id);
 
-    await Task.findByIdAndDelete(taskId);
+    await Task.findByIdAndDelete(task._id);
 
-     return res.status(200).json(new ApiResponse(200 , {} , "Task deleted  successfully..."))
+    return res.status(200).json(new ApiResponse(200 , {} , "Task deleted  successfully..."))
 
 });
 
 const toggleTaskStatus = asyncHandler(async (req , res) => {
-   const taskId = req.params.id;
+   const taskId = req.params.taskId;
     if(!taskId){
         throw new ApiError(404 , "Task Id not found...")
     }
 
-    const task = await Task.findById(taskId)
-    if(!task){
-        throw new ApiError(404 , "Task does not exists...")
-    }
-
-    await validateProject(req.params.projectId , req.user._id);
+    const task = await validateTask(taskId , req.user._id);
 
     task.completed = !task.completed
     await task.save()
