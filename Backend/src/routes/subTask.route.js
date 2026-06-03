9c122693ffa32ccc8a5fd55e21009187656ce330
Shivam <choudharyshivam0508@gmail.com>
2026-06-03 19:57:25 +0530

    feat: add sub-task management routes for creating, retrieving, updating, and toggling status

--- Backend/src/routes/subTask.route.js
+++ Backend/src/routes/subTask.route.js
@@ -1,5 +1,26 @@
 import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+  createSubTask,
+  getSubTaskById,
+  getSubTasksByTask,
+  updateSubTask,
+  deleteSubTask,
+  toggleSubTaskStatus,
+} from "../controller/subTask.controller.js";
 
 const router = Router();
+
+router
+  .route("/task/:taskId")
+  .post(verifyJWT, createSubTask)
+  .get(verifyJWT, getSubTasksByTask);
+
+router.route("/:subTaskId")
+  .get(verifyJWT, getSubTaskById)
+  .patch(verifyJWT, updateSubTask)
+  .delete(verifyJWT, deleteSubTask);
+
+router.route("/subTaskId/toogle-status").patch(verifyJWT , toggleSubTaskStatus)
 
 export default router;
