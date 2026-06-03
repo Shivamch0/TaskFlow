0e388b142fa068d5db8f79b8b53171d0f8511b02
Shivam <choudharyshivam0508@gmail.com>
2026-06-03 12:19:52 +0530

    feat: implement user authentication controller and routes with JWT token management

--- Backend/src/controller/user.controller.js
+++ Backend/src/controller/user.controller.js
@@ -176,4 +176,4 @@ const refreshAccessToken = asyncHandler(async (req , res) => {
     }
 });
 
-export { registerUser, loginUser , logoutUser };
+export { registerUser, loginUser , logoutUser , currentUser , refreshAccessToken };
--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
-import { registerUser , loginUser } from "../controller/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { registerUser , loginUser, logoutUser  , currentUser , refreshAccessToken} from "../controller/user.controller.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
 
 router.route("/login").post(loginUser)
+
+//? Auth routes
+router.route("/logout").post(verifyJWT , logoutUser);
+
+router.route("/refresh-token").post(verifyJWT , refreshAccessToken)
+
+router.route("/current-user").get(verifyJWT , currentUser )
 
 export default router;
